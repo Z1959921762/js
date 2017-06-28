fb6e6b8a8681967772dcd2a8301a98884b425398
Z1959921762 <tangbinzhong1@163.com>
2017-06-28 13:52:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('123)
+console.log("meteor.js手机分享照片:docs.meteor.com");
+console.log("html5.js‘使html5兼容IE系列’：
+            <!–[if IE]>  
+<script src=”http://html5shiv.googlecode.com/svn/trunk/html5.js”></script>  
+<![endif]–>");
+console.log("layer.js‘弹出层’：layer.layui.com");
+console.log("h5‘3D动画’:www.html5tricks.com");
